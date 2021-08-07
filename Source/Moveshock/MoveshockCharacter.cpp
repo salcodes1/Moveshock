a3177e215f00df9b42c11cdae696be45a686c99d
rexx <67599507+r-ex@users.noreply.github.com>
2021-08-07 20:42:00 +0100

    removed wallrun and slide

--- Source/Moveshock/MoveshockCharacter.cpp
+++ Source/Moveshock/MoveshockCharacter.cpp
@@ -49,54 +49,6 @@ void AMoveshockCharacter::StopJumping()
 void AMoveshockCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	TArray<FHitResult> SphereHits;
-
-	GetWorld()->SweepMultiByChannel(SphereHits, GetActorLocation(), GetActorLocation() + FVector(10), FQuat::Identity, ECC_WorldStatic, FCollisionShape::MakeSphere(200));
-	
-	static float lastWallrunTime = 0;
-	
-	FHitResult FinalHit;
-		
-	bIsWallrunning = false;
-
-	for(FHitResult& SphereHit : SphereHits)
-	{
-		// if(!IsValid(SphereHit.GetComponent()))
-		// 	GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Red, TEXT("BRUH"));
-		// else
-		// 	GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Red, SphereHit.GetActor()->GetName());
-		if(IsValid(SphereHit.GetComponent()) && SphereHit.GetActor() != this)
-		{
-			if(GetCharacterMovement()->IsFalling())
-			{
-					bIsWallrunning = true;
-					FinalHit = SphereHit;
-			}
-			break;
-		}
-	}
-	if(bWantsToJump && bIsWallrunning)
-	{
-		bIsWallrunning = false;
-		GetCharacterMovement()->AddImpulse((FinalHit.ImpactNormal + 1.2 * FVector::UpVector) * 200);
-	}
-	
-	if(bIsWallrunning)
-	{
-		Camera->SetFieldOfView(115.f);
-		GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Red, "ADDING");
-		GetCharacterMovement()->AddForce(FVector::CrossProduct(FinalHit.ImpactNormal, FVector(0, 1, 0)) * 2);
-		DrawDebugLine(GetWorld(), FinalHit.ImpactPoint, FinalHit.ImpactPoint + FVector::CrossProduct(FinalHit.ImpactNormal, FVector(0, 1, 0)) * 200, FColor::Cyan, false, 100);
-		// DrawDebugLine(GetWorld(), FinalHit.ImpactPoint, (FinalHit.ImpactPoint + FinalHit.ImpactNormal).BackwardVector * 400, FColor::Red, false, 100);
-		// DrawDebugLine(GetWorld(), FinalHit.Location, FinalHit.Location + FinalHit.ImpactNormal.BackwardVector * 200, FColor::Red, false, 100);
-		// DrawDebugLine(GetWorld(), FinalHit.Location, FinalHit.Location + FinalHit.ImpactNormal.LeftVector * 400, FColor::Green, false, 100);
-		if(GetVelocity().Z < 0)
-			GetCharacterMovement()->AddForce(FVector(0, 0, -1 * GetVelocity().Z) * 100);
-		lastWallrunTime = FPlatformTime::ToMilliseconds(FPlatformTime::Cycles());
-	}
-	else Camera->SetFieldOfView(110.f);
-	
 }
 
 // Called to bind functionality to input
@@ -142,6 +94,5 @@ void AMoveshockCharacter::StopCrouchSlide()
 
 void AMoveshockCharacter::Slide()
 {
-	GetCharacterMovement()->AddImpulse(GetVelocity() * 5, true);
-	GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Red, "HELOOOO");
+
 }
